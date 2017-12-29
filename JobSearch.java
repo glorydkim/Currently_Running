4ab91c7ba985d74983e3958979ef19baf84bff07
Glory Kim <glorydkim@gmail.com>
2017-12-29 16:16:53 -0500

    Create JobSearch.java

---
+++ JobSearch.java
@@ -0,0 +1,13 @@
+public class JobSearch {
+
+    public static void main(String[] args) {
+        
+        boolean needExperience = true;
+        boolean haveExperience = false;
+        while(haveExperience == false){
+            System.out.println("Need a job to get experience? " + needExperience);
+            System.out.println("Have experience? " + haveExperience);
+            System.out.println("Need Experience to get Job");
+        }
+    }   
+}
